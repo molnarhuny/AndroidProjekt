7f8a3086d095de287e590017bf8cfebcb3b9f23d
Molnar Hunor <44946379+molnarhuny@users.noreply.github.com>
2022-12-29 20:57:34 +0200

    Created TasksScreen layout

--- app/src/main/java/com/example/projekt/api/fragments/TasksFragment.kt
+++ app/src/main/java/com/example/projekt/api/fragments/TasksFragment.kt
@@ -7,6 +7,8 @@ import android.view.View
 import android.view.ViewGroup
 import com.example.projekt.R
 
+// @TODO - Rounded Corner on ImageView
+
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
 private const val ARG_PARAM1 = "param1"
